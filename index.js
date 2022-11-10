ab35ec31bca6945d9c308850d06fd3b403477dd3
Rishi Pratap <rp0258@srmist.edu.in>
2022-11-10 14:17:40 +0530

    fix bug

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ app.use(cors(corsOptions));
 
 const manager = new NlpManager({ languages: ["en"] });
 manager.load();
-var readline = require("readline");
-var rl = readline.createInterface(process.stdin, process.stdout);
 
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,23 +27,16 @@ app.get('/', (req, res) => {
 console.log("Starting Chatbot ...");
 
 console.log("Chatbot started!");
-rl.setPrompt("> ");
-rl.prompt();
 
  app.post('/messenger', (req, res) => {
 		const book = req.body;
 		// Output the book to the console for debugging
 		console.log("response from client: ",book.sender);
 		var msg = book.sender;
-		rl.prompt();
 		async function run() {
 		const response = await manager.process("en", msg);
 		console.log("NLP said: ",response.answer);
 		res.send(response.answer);
-		rl.prompt();
-		rl.on("close", function () {
-		process.exit(0);
-	});
 }
 run();
 });
